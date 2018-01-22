787bb9106d445da8e28c1f31bfce3be7bacf17a0
Chris Knight Johnson and Luke Bertram <chris@>
2018-01-22 11:34:08 -0800

    Add main component and setup router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
-import { ModuleWithProviders }  from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const appRoutes: Routes = [
   {
